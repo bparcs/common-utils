abb50560b00f5acdce5555c70a9a377e676c8cd1
Brian Parco <parcob@hotmail.com>
2021-08-28 08:26:35 -0400

    initial commit

---
+++ CommonUtils/LoggingService/Interfaces/ILoggingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.LoggingService.Interfaces
+{
+    public interface ILoggingService
+    {
+        void Information(string message);
+        void Debug(string message);
+        void Warning(string message);
+        void Error(string message);
+    }
+}
---
+++ CommonUtils/LoggingService/LoggingService.cs
@@ -0,0 +1,38 @@
+﻿using CommonUtils.LoggingService.Interfaces;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.LoggingService
+{
+    public class LoggingService : ILoggingService
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public LoggingService()
+        {
+
+        }
+
+        public void Debug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void Information(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
